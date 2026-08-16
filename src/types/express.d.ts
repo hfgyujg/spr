c3f6926750adb47642514cdc1d19a92c693910eb
hfgyujg <KEITHISRICH12@OUTLOOK.COM>
2026-08-16 14:05:37 -0700

    fix: add SPR Connect request typing

---
+++ src/types/express.d.ts
@@ -0,0 +1,12 @@
+import 'express-serve-static-core';
+
+declare module 'express-serve-static-core' {
+  interface Request {
+    sprApi?: {
+      id: string;
+      tenantId: string;
+      name: string;
+      scopes: string[];
+    };
+  }
+}
